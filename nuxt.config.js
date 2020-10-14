7ea1aa0262971c7bc949dc75a4129258460fd139
Luis Bauza <hllywluis2@me.com>
2020-10-14 17:48:15 -0400

    Updated firestore.rules, nuxt config and functions package.json

--- nuxt.config.js
+++ nuxt.config.js
@@ -26,8 +26,8 @@ export default {
     script: [
       {
         crossOrigin: 'anonymous',
-        integrity: 'sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=',
-        src: 'https://code.jquery.com/jquery-3.5.1.js'
+        integrity: 'sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=',
+        src: 'https://code.jquery.com/jquery-3.5.1.min.js'
       },
       {
         crossOrigin: 'anonymous',
